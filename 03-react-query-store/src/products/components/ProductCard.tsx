f9a80cba262ca3babf357f1ba5beb2d532c6c2c0
Franzua Plasencia <franzua.15pc@gmail.com>
2024-09-07 11:21:10 -0500

    style(products): change image size

--- 03-react-query-store/src/products/components/ProductCard.tsx
+++ 03-react-query-store/src/products/components/ProductCard.tsx
@@ -8,7 +8,7 @@ export const ProductCard = () => {
           src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
           alt="tailwind logo"
           width={300}
-          height={400}
+          height={300}
           className="rounded-xl p-5 sm:p-0 bg-white"
         />
       </div>
